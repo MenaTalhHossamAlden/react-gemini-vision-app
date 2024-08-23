0abe15b41c29271376a68ef3520b4ffd3efa9072
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-23 11:28:35 +0300

    add UI elements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import {useState} from "react";
-import {upload} from "@testing-library/user-event/dist/upload";
 const App = () => {
     const [image, setImage] = useState(null);
+    const [value, setValue] = useState("");
+    const [response, setResponse] = useState("");
+    const [error, setError] = useState("");
+
+    const surpriseOptions = [
+        "Does the image have a whale?",
+        "Is the image fabulously pink?",
+        "Does the image have puppies?"
+    ]
+
+    const surprise = () => {
+        const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+        setValue(randomValue)
+    }
 
     const uploadImage = async (e) => {
         const formData = new FormData()
@@ -20,16 +33,46 @@ const App = () => {
         }
     }
 
-    console.log(image)
+    const analyzerImage = () => {
+
+    }
+
+    const clear = () => {
+        setImage(null)
+        setValue("")
+        setResponse("")
+        setError("")
+    }
 
   return (
-    <div className="app">
-        {image&&
-            <img src={URL.createObjectURL(image)} alt=""/>
-        }
-      <label htmlFor="files"> upload an image </label>
-      <input id="files" type="file" accept="image/*" hidden onChange={(e)=>uploadImage(e)} />
-    </div>
+      <div className="app">
+          <section className="search-section">
+              <div className="image-container">
+                  {image && <img className="image" src={URL.createObjectURL(image)} alt=""/>}
+              </div>
+              <p className="extra-info">
+                  <span>
+                      <label htmlFor="files"> upload an image </label>
+                      <input id="files" type="file" accept="image/*" hidden onChange={uploadImage}/>
+                  </span>
+                  to ask questions about.
+              </p>
+              <p>What do you what to know about the image?
+                  <button className="surprise" onClick={surprise} disabled={response}>Surprise me</button>
+              </p>
+              <div className="input-container">
+                  <input
+                      placeholder="What is in the image..."
+                      value={value}
+                      onChange={e => setValue(e.target.value)}
+                  />
+                  {(!response && !error) && <button onClick={analyzerImage}>Ask me</button>}
+                  {(response || error) && <button onClick={clear}>Clear</button>}
+              </div>
+              {error && <p>{error}</p>}
+              {response && <p>{response }</p>}
+          </section>
+      </div>
   );
 }
 
